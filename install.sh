28523d8db1acb2638ea9fcfde45186295833b4d6
kletellier <gregory.letellier@gmail.com>
2016-01-28 11:12:59 +0100

    Big Bang

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+ 
+## Main Paths
+SETUP_PATH="${PWD}"
+BIN_PATH="${PWD}/bin"
+SAMPLE_PATH="${PWD}/samples"
+echo "Install Cpc Tools"
+echo "Binary folder : ${BIN_PATH}"
+echo "Sample folder : ${SAMPLE_PATH}"
+echo "Clear folders"
+rm -rf $BIN_PATH
+rm -rf bin CapriceRPI iDSK-0.13 pasmo-0.5.3
+echo "Create Folders"
+mkdir $BIN_PATH
+cd $SETUP_PATH
+echo "Install CapriceRpi2 emulator"
+# install CapriceRpi2 
+apt-get install -y git build-essential libsdl-image1.2-dev libsdl-ttf2.0-dev
+git clone https://github.com/kletellier/CapriceRPI.git --branch HiRes4All
+cd CapriceRPI/src
+make
+cp capriceRPI $BIN_PATH
+
+cd $SETUP_PATH
+echo "Install iDsk Tools for creating disk image"
+# install iDsk
+
+tar -xzvf iDSK-0.13.tgz 
+cd $SETUP_PATH/iDSK-0.13
+make
+cp $SETUP_PATH/iDSK-0.13/bin/iDSK $BIN_PATH
+rm -rf $SETUP_PATH/iDSK-0.13
+
+cd $SETUP_PATH
+echo "Install pasmo for assembling asm code"
+# install Pasmo
+tar -xzvf pasmo-0.5.3.tgz
+cd $SETUP_PATH/pasmo-0.5.3
+./configure
+make
+cp pasmo $BIN_PATH
+rm -rf $SETUP_PATH/pasmo-0.5.3
+# compile sample
+cd $BIN_PATH
+# assemble hello world file
+./pasmo $SAMPLE_PATH/helloworld.asm $SAMPLE_PATH/hw.bin
+# create disk file
+./iDSK $SAMPLE_PATH/hello.dsk -n -i $SAMPLE_PATH/hw.bin -e 4000 -c 4000 -t 1
